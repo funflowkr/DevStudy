e949d6a01f708338959b9e542e8fe102daf1be95
tuchoi00 <tuchoi@funflow.co.kr>
2019-11-27 10:56:54 +0900

    LWRP ShaderGraph 예제 추가

---
+++ Unity/Assets/Script/ScriptableRenderPipeline/LWRP/ShaderGraphController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderGraphController : MonoBehaviour
+{
+    float displacementAmount = 0f;
+    MeshRenderer meshRenderer;
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime);
+        meshRenderer.material.SetFloat("_Amount", displacementAmount);
+
+        if (Input.GetMouseButtonDown(0))
+            displacementAmount += 1;
+    }
+}
