a08b000191aabcb070714dbe1b09bdc48e2ab1b5
권영달 <jiff100@funflow.co.kr>
2019-11-28 18:47:01 +0900

    c# 7.2

---
+++ Unity/Assets/Script/CSharp7.2/OutVariable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class OutVariable : MonoBehaviour
+{
+    void Start()
+    {
+        /// 별도의 선언없이 out 파라메터를 사용 가능.
+		if (int.TryParse("10", out int result))
+			Debug.Log(result);
+		else
+			Debug.Log("Could not parse input");
+	}
+
+}
