3111c2a45bd3ed707fb9b27d0e7b68ac691bb17b
권영달 <jiff100@funflow.co.kr>
2019-11-29 10:36:43 +0900

    c# 7 - Tuples

--- Unity/Assets/Script/CSharp7.2/TuplesTestController.cs
+++ Unity/Assets/Script/CSharp7.2/TuplesTestController.cs
@@ -2,17 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// https://docs.microsoft.com/ko-kr/dotnet/csharp/tuples
+/// 
+/// generic Tuple classes
+/// - Named their properties Item1, Item2, and so on.
+/// - Low performance because they are reference types.
+/// 
+/// </summary>
 public class TuplesTestController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnnamedTuples();
+        NamedTuples();
+        TupleProjectionInitializers();
+        Compare();
+        NestedTuples();
+        MethodReturnValues();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UnnamedTuples()
     {
-        
+        var unnamedTuple = (5, 12.5);
+        Debug.Log($"Unnamed Tuples : {unnamedTuple.Item1}, {unnamedTuple.Item2}");
+    }
+
+    void NamedTuples()
+    {
+        var named = (first: "one", second: "two");
+        Debug.Log($"Named Tuples :{named.first}, {named.second}");
+
+        var sum = 12.5;
+        var count = 5;
+        var accumulation = (count, sum);
+
+        Debug.Log($"Named Tuples :{accumulation.count}, {accumulation.sum}");
+    }
+
+    void TupleProjectionInitializers()
+    {
+        var localVariableOne = 5;
+        var localVariableTwo = "some text";
+
+        var tuple = (explicitFieldOne: localVariableOne, explicitFieldTwo: localVariableTwo);
+
+        Debug.Log($"{tuple.explicitFieldOne}, {tuple.explicitFieldTwo}");
+    }
+
+    void Compare()
+    {
+        var left = (a: 5, b: 10);
+        var right = (a: 5, b: 10);
+        (int a, int b)? nullableTuple = right;
+        Debug.Log(left == right); // displays 'true'
+    }
+
+    void NestedTuples()
+    {
+        (int, (int, int)) nestedTuple = (1, (2, 3));
+        Debug.Log(nestedTuple == (1, (2, 3)));
+    }
+
+    void MethodReturnValues()
+    {
+        var result = ReturnNestedTuples();
+        Debug.Log($"{result.Item1}, {result.Item2.Item1}, {result.Item2.Item2}");
+    }
+
+    (int, (int, int)) ReturnNestedTuples()
+    {
+        return (1, (2, 3));
     }
 }
