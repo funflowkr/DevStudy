aed95fdff153b9d9b3c8b613c2e2927d4a9aac23
이상협 <sanghyub@funflow.co.kr>
2019-12-02 18:28:05 +0900

    Unity2019 프로젝트 추가

---
+++ Unity2019/Assets/Script/ECS/FindTargetSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using System.Linq;
+
+public class FindTargetSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+
+        //World.Active.GetOrCreateManager<EntityManager>().Get
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+        var units = entityManager.GetAllEntities().Where( r => entityManager.HasComponent<Unit>(r));
+        var targets = entityManager.GetAllEntities().Where(r => entityManager.HasComponent<Target>(r));
+
+        foreach (var unit in units)
+        {
+            Debug.Log("Unit " + unit);
+            Entity closestTargetEntity = Entity.Null;
+
+            foreach (var target in targets)
+            {
+                Debug.Log("Target " + target);
+
+                if (closestTargetEntity == Entity.Null)
+                {
+                    closestTargetEntity = target;
+                }
+                else
+                {
+                    //if (math.distance())
+                }
+            }
+        }
+
+        
+    }
+}
