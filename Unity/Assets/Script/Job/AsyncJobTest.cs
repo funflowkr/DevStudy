28d6eca89310209f7a6d7cb899aea0d6f124d87f
권영달 <jiff100@funflow.co.kr>
2020-02-17 15:52:00 +0900

    컴파일 오류 임시 주석 처리

--- Unity/Assets/Script/Job/AsyncJobTest.cs
+++ Unity/Assets/Script/Job/AsyncJobTest.cs
@@ -1,107 +1,109 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Jobs;
-using Unity.Jobs;
-using Unity.Collections;
-using Unity.Mathematics;
-using Unity.Entities;
-using Unity.Burst;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Jobs;
+//using Unity.Jobs;
+//using Unity.Collections;
+//using Unity.Mathematics;
+//using Unity.Entities;
+//using Unity.Burst;
+//using System.Threading.Tasks;
 
-public class AsyncJobTest : MonoBehaviour
-{
-    public static int MainThreadID = 0;
-    public class Cube
-    {
-        public Transform Form;
-        public float MoveY;
-    }
-    
-    private List<Cube> _cubeList = new List<Cube>();
+// 컴파일 오류로 임시로 주석 처리
 
-    public const float MIN_VALUE = -5f;
-    public const float MAX_VALUE = 5f;
+//public class AsyncJobTest : MonoBehaviour
+//{
+//    public static int MainThreadID = 0;
+//    public class Cube
+//    {
+//        public Transform Form;
+//        public float MoveY;
+//    }
 
-    private void Start()
-    {
-        MainThreadID = System.Threading.Thread.CurrentThread.ManagedThreadId;
-        Debug.Log(MainThreadID);
-        for (int i = 0; i < 10; i++)
-        {
-            GameObject cubeGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cubeGO.transform.position = new Vector3(UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE), UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE), 0f);
-            cubeGO.transform.localScale = Vector3.one * 0.4f;
+//    private List<Cube> _cubeList = new List<Cube>();
 
-            Cube cube = new Cube()
-            {
-                Form = cubeGO.transform,
-                MoveY = UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE)
-            };
-            _cubeList.Add(cube);
-        }
+//    public const float MIN_VALUE = -5f;
+//    public const float MAX_VALUE = 5f;
 
-    }
+//    private void Start()
+//    {
+//        MainThreadID = System.Threading.Thread.CurrentThread.ManagedThreadId;
+//        Debug.Log(MainThreadID);
+//        for (int i = 0; i < 10; i++)
+//        {
+//            GameObject cubeGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//            cubeGO.transform.position = new Vector3(UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE), UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE), 0f);
+//            cubeGO.transform.localScale = Vector3.one * 0.4f;
 
-    private void Update()
-    {
-        foreach(var cube in _cubeList)
-            cube.Form.Rotate(Vector3.forward * Time.deltaTime * 30f);
+//            Cube cube = new Cube()
+//            {
+//                Form = cubeGO.transform,
+//                MoveY = UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE)
+//            };
+//            _cubeList.Add(cube);
+//        }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            AsyncParallelMoveJob job = new AsyncParallelMoveJob();
+//    }
 
-            JobHandle handler = job.Schedule(_cubeList.Count, _cubeList.Count / 10); /// innerloopBatchCount는 잡 워커가 가져갈 반복 횟수??
+//    private void Update()
+//    {
+//        foreach(var cube in _cubeList)
+//            cube.Form.Rotate(Vector3.forward * Time.deltaTime * 30f);
 
-            /*https://forum.unity.com/threads/burst-and-thread-safe-api-outside-unity-jobs-system.522737/
-             * Schedule doesn't actually schedule the jobs immediately, but add them to a queue. 
-             * Jobs are scheduled when you call JobHandle.ScheduleBatchedJobs or JobHandle.Complete. 
-             * This is done for performance reasons since scheduling individual jobs results in expensive Semaphore.Signal calls. 
-             * By scheduling many jobs at the same time delayed this cost will instead be paid only once per ScheduleBatchedJobs calls.*/
-            handler.Complete();
-        }
-    }
-}
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            AsyncParallelMoveJob job = new AsyncParallelMoveJob();
 
-public struct AsyncParallelMoveJob : IJobParallelFor
-{
-    int tempIndex;
+//            JobHandle handler = job.Schedule(_cubeList.Count, _cubeList.Count / 10); /// innerloopBatchCount는 잡 워커가 가져갈 반복 횟수??
 
-    public void Execute(int index)
-    {
-        tempIndex = index;
-        //Process();
-        Task.Factory.StartNew(AsyncProcess);
-    }
+//            /*https://forum.unity.com/threads/burst-and-thread-safe-api-outside-unity-jobs-system.522737/
+//             * Schedule doesn't actually schedule the jobs immediately, but add them to a queue. 
+//             * Jobs are scheduled when you call JobHandle.ScheduleBatchedJobs or JobHandle.Complete. 
+//             * This is done for performance reasons since scheduling individual jobs results in expensive Semaphore.Signal calls. 
+//             * By scheduling many jobs at the same time delayed this cost will instead be paid only once per ScheduleBatchedJobs calls.*/
+//            handler.Complete();
+//        }
+//    }
+//}
 
-    async void AsyncProcess()
-    {
-        float v = 0f;
-        int i = 0;
-        Debug.LogFormat("Index : {0} AsyncProcess job Start, ThreadID : {1} ", tempIndex, System.Threading.Thread.CurrentThread.ManagedThreadId);
-        for (i = 0; i < 1000000; i++)
-        {
-            v = math.exp10(math.sqrt(v));
-            if(i != 0 && i % 10000 == 0)
-                await Task.Delay(25);
-        }
-        Debug.LogFormat("Index : {0} AsyncProcess job End, ThreadID : {1} ", tempIndex, System.Threading.Thread.CurrentThread.ManagedThreadId);
-        
-    }
+//public struct AsyncParallelMoveJob : IJobParallelFor
+//{
+//    int tempIndex;
 
-    void Process()
-    {
-        float v = 0f;
-        int i = 0;
-        Debug.LogFormat("Index : {0} Process job Start, ThreadID : {1} ", tempIndex, System.Threading.Thread.CurrentThread.ManagedThreadId);
-        for (i = 0; i < 1000000; i++)
-        {
-            v = math.exp10(math.sqrt(v));
-        }
-        Debug.LogFormat("Index : {0} Process job End, ThreadID : {1} ", tempIndex, System.Threading.Thread.CurrentThread.ManagedThreadId);
-    }
-}
+//    public void Execute(int index)
+//    {
+//        tempIndex = index;
+//        //Process();
+//        Task.Factory.StartNew(AsyncProcess);
+//    }
+
+//    async void AsyncProcess()
+//    {
+//        float v = 0f;
+//        int i = 0;
+//        Debug.LogFormat("Index : {0} AsyncProcess job Start, ThreadID : {1} ", tempIndex, System.Threading.Thread.CurrentThread.ManagedThreadId);
+//        for (i = 0; i < 1000000; i++)
+//        {
+//            v = math.exp10(math.sqrt(v));
+//            if(i != 0 && i % 10000 == 0)
+//                await Task.Delay(25);
+//        }
+//        Debug.LogFormat("Index : {0} AsyncProcess job End, ThreadID : {1} ", tempIndex, System.Threading.Thread.CurrentThread.ManagedThreadId);
+
+//    }
+
+//    void Process()
+//    {
+//        float v = 0f;
+//        int i = 0;
+//        Debug.LogFormat("Index : {0} Process job Start, ThreadID : {1} ", tempIndex, System.Threading.Thread.CurrentThread.ManagedThreadId);
+//        for (i = 0; i < 1000000; i++)
+//        {
+//            v = math.exp10(math.sqrt(v));
+//        }
+//        Debug.LogFormat("Index : {0} Process job End, ThreadID : {1} ", tempIndex, System.Threading.Thread.CurrentThread.ManagedThreadId);
+//    }
+//}
 
 
--- Unity/Assets/Script/Job/MoveJobTest.cs
+++ Unity/Assets/Script/Job/MoveJobTest.cs
@@ -1,151 +1,153 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Jobs;
-using Unity.Jobs;
-using Unity.Collections;
-using Unity.Mathematics;
-using Unity.Entities;
-using Unity.Burst;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Jobs;
+//using Unity.Jobs;
+//using Unity.Collections;
+//using Unity.Mathematics;
+//using Unity.Entities;
+//using Unity.Burst;
 
-/*
- * 잡시스템 요약 링크
- * https://unity3dtuts.com/c-sharp-job-system-in-unity/
- */
+///*
+// * 잡시스템 요약 링크
+// * https://unity3dtuts.com/c-sharp-job-system-in-unity/
+// */
 
-public class MoveJobTest : MonoBehaviour
-{
-    public class Cube
-    {
-        public Transform Form;
-        public float MoveY;
-    }
+// 컴파일 오류로 임시로 주석 처리
 
-    public bool isUseJob;
+//public class MoveJobTest : MonoBehaviour
+//{
+//    public class Cube
+//    {
+//        public Transform Form;
+//        public float MoveY;
+//    }
 
-    private List<Cube> _cubeList = new List<Cube>();
+//    public bool isUseJob;
 
-    public const float MIN_VALUE = -5f;
-    public const float MAX_VALUE = 5f;
+//    private List<Cube> _cubeList = new List<Cube>();
 
-    private void Start()
-    {
-        for(int i = 0 ; i < 1000 ; i++)
-        {
-            GameObject cubeGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cubeGO.transform.position = new Vector3(UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE), UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE), 0f);
-            cubeGO.transform.localScale = Vector3.one * 0.4f;
+//    public const float MIN_VALUE = -5f;
+//    public const float MAX_VALUE = 5f;
 
-            Cube cube = new Cube()
-            {
-                Form = cubeGO.transform,
-                MoveY = UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE)
-            };
-            _cubeList.Add(cube);
-        }
-    }
+//    private void Start()
+//    {
+//        for(int i = 0 ; i < 1000 ; i++)
+//        {
+//            GameObject cubeGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//            cubeGO.transform.position = new Vector3(UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE), UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE), 0f);
+//            cubeGO.transform.localScale = Vector3.one * 0.4f;
 
-    private void Update()
-    {
-        if(isUseJob)
-        {
-            NativeList<float3> positionList = new NativeList<float3>(Allocator.TempJob);
-            NativeList<float> moveYList = new NativeList<float>(Allocator.TempJob);
-            /// string을 Job으로 넘기는 법
-            string s = "Test";
-            NativeArray<byte> byteList = new NativeArray<byte>(s.Length, Allocator.TempJob);
-            byteList.CopyFrom(System.Text.Encoding.ASCII.GetBytes(s));
+//            Cube cube = new Cube()
+//            {
+//                Form = cubeGO.transform,
+//                MoveY = UnityEngine.Random.Range(MIN_VALUE, MAX_VALUE)
+//            };
+//            _cubeList.Add(cube);
+//        }
+//    }
 
-            for (int i = 0; i < _cubeList.Count; i++)
-            {
-                positionList.Add(_cubeList[i].Form.position);
-                moveYList.Add(_cubeList[i].MoveY);
-            }
+//    private void Update()
+//    {
+//        if(isUseJob)
+//        {
+//            NativeList<float3> positionList = new NativeList<float3>(Allocator.TempJob);
+//            NativeList<float> moveYList = new NativeList<float>(Allocator.TempJob);
+//            /// string을 Job으로 넘기는 법
+//            string s = "Test";
+//            NativeArray<byte> byteList = new NativeArray<byte>(s.Length, Allocator.TempJob);
+//            byteList.CopyFrom(System.Text.Encoding.ASCII.GetBytes(s));
 
-            ParallelMoveJob job = new ParallelMoveJob()
-            {
-                PositionArray = positionList,
-                MoveYArray = moveYList,
-                StrByteArray = byteList,
-                deltaTime = Time.deltaTime
-            };
+//            for (int i = 0; i < _cubeList.Count; i++)
+//            {
+//                positionList.Add(_cubeList[i].Form.position);
+//                moveYList.Add(_cubeList[i].MoveY);
+//            }
+
+//            ParallelMoveJob job = new ParallelMoveJob()
+//            {
+//                PositionArray = positionList,
+//                MoveYArray = moveYList,
+//                StrByteArray = byteList,
+//                deltaTime = Time.deltaTime
+//            };
 
 
-            JobHandle handler = job.Schedule(_cubeList.Count, _cubeList.Count / 10); /// innerloopBatchCount는 잡 워커가 가져갈 반복 횟수??
+//            JobHandle handler = job.Schedule(_cubeList.Count, _cubeList.Count / 10); /// innerloopBatchCount는 잡 워커가 가져갈 반복 횟수??
 
-            /*https://forum.unity.com/threads/burst-and-thread-safe-api-outside-unity-jobs-system.522737/
-             * Schedule doesn't actually schedule the jobs immediately, but add them to a queue. 
-             * Jobs are scheduled when you call JobHandle.ScheduleBatchedJobs or JobHandle.Complete. 
-             * This is done for performance reasons since scheduling individual jobs results in expensive Semaphore.Signal calls. 
-             * By scheduling many jobs at the same time delayed this cost will instead be paid only once per ScheduleBatchedJobs calls.*/
-            handler.Complete();
+//            /*https://forum.unity.com/threads/burst-and-thread-safe-api-outside-unity-jobs-system.522737/
+//             * Schedule doesn't actually schedule the jobs immediately, but add them to a queue. 
+//             * Jobs are scheduled when you call JobHandle.ScheduleBatchedJobs or JobHandle.Complete. 
+//             * This is done for performance reasons since scheduling individual jobs results in expensive Semaphore.Signal calls. 
+//             * By scheduling many jobs at the same time delayed this cost will instead be paid only once per ScheduleBatchedJobs calls.*/
+//            handler.Complete();
 
-            for (int i = 0 ; i < _cubeList.Count ; i++)
-            {
-                _cubeList[i].Form.position = job.PositionArray[i];
-            }
+//            for (int i = 0 ; i < _cubeList.Count ; i++)
+//            {
+//                _cubeList[i].Form.position = job.PositionArray[i];
+//            }
 
-            positionList.Dispose();
-            moveYList.Dispose();
-            byteList.Dispose();
-        }
-        else
-        {
-            foreach(var cube in _cubeList)
-            {
-                Vector3 temp = cube.Form.position + Vector3.up * cube.MoveY * Time.deltaTime;
+//            positionList.Dispose();
+//            moveYList.Dispose();
+//            byteList.Dispose();
+//        }
+//        else
+//        {
+//            foreach(var cube in _cubeList)
+//            {
+//                Vector3 temp = cube.Form.position + Vector3.up * cube.MoveY * Time.deltaTime;
 
-                if (temp.y < MIN_VALUE)
-                    temp.y = MAX_VALUE;
-                else if (temp.y > MAX_VALUE)
-                    temp.y = MIN_VALUE;
+//                if (temp.y < MIN_VALUE)
+//                    temp.y = MAX_VALUE;
+//                else if (temp.y > MAX_VALUE)
+//                    temp.y = MIN_VALUE;
 
-                cube.Form.position = temp;
+//                cube.Form.position = temp;
 
-                /// cpu를 많이 사용하는 코드
-                float v = 0f;
-                for(int i = 0 ; i < 1000 ; i++)
-                {
-                    v = math.exp10(math.sqrt(v));
-                }
-            }
-        }
-    }
-}
+//                /// cpu를 많이 사용하는 코드
+//                float v = 0f;
+//                for(int i = 0 ; i < 1000 ; i++)
+//                {
+//                    v = math.exp10(math.sqrt(v));
+//                }
+//            }
+//        }
+//    }
+//}
 
-[BurstCompile]
-public struct ParallelMoveJob : IJobParallelFor
-{
-    // 잡 시스템에서는 참조타입을 쓸 수가 없어서 무조건 값형식으로만 쓸 수 있다.
-    // 배열을 쓰려면 네이티브 컨테이너 NativeArray를 사용해야한다.
+//[BurstCompile]
+//public struct ParallelMoveJob : IJobParallelFor
+//{
+//    // 잡 시스템에서는 참조타입을 쓸 수가 없어서 무조건 값형식으로만 쓸 수 있다.
+//    // 배열을 쓰려면 네이티브 컨테이너 NativeArray를 사용해야한다.
 
-    public NativeArray<float3> PositionArray;
-    public NativeArray<float> MoveYArray;
-    public NativeArray<byte> StrByteArray;
-    public float deltaTime;// 잡 시스템에서는 Time.deltaTime에 접근이 불가능 메인쓰레드를 사용한다는 보장이 없기 때문?
+//    public NativeArray<float3> PositionArray;
+//    public NativeArray<float> MoveYArray;
+//    public NativeArray<byte> StrByteArray;
+//    public float deltaTime;// 잡 시스템에서는 Time.deltaTime에 접근이 불가능 메인쓰레드를 사용한다는 보장이 없기 때문?
 
-    public void Execute(int index)
-    {
-        float3 up = new float3(0f, 1f, 0f);
-        Vector3 temp = PositionArray[index] + up * MoveYArray[index] * deltaTime;
+//    public void Execute(int index)
+//    {
+//        float3 up = new float3(0f, 1f, 0f);
+//        Vector3 temp = PositionArray[index] + up * MoveYArray[index] * deltaTime;
 
-        if (temp.y < MoveJobTest.MIN_VALUE)
-            temp.y = MoveJobTest.MAX_VALUE;
-        else if (temp.y > MoveJobTest.MAX_VALUE)
-            temp.y = MoveJobTest.MIN_VALUE;
+//        if (temp.y < MoveJobTest.MIN_VALUE)
+//            temp.y = MoveJobTest.MAX_VALUE;
+//        else if (temp.y > MoveJobTest.MAX_VALUE)
+//            temp.y = MoveJobTest.MIN_VALUE;
 
-        PositionArray[index] = temp;
+//        PositionArray[index] = temp;
 
-        /// cpu를 많이 사용하는 코드
-        float v = 0f;
-        for (int i = 0; i < 1000; i++)
-        {
-            v = math.exp10(math.sqrt(v));
-        }
-        string s = System.Text.Encoding.ASCII.GetString(StrByteArray.ToArray());
-        //Debug.Log(s);
-    }
-}
+//        /// cpu를 많이 사용하는 코드
+//        float v = 0f;
+//        for (int i = 0; i < 1000; i++)
+//        {
+//            v = math.exp10(math.sqrt(v));
+//        }
+//        string s = System.Text.Encoding.ASCII.GetString(StrByteArray.ToArray());
+//        //Debug.Log(s);
+//    }
+//}
 
 
