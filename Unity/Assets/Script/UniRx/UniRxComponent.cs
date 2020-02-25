dcd448d8d4a81288128481ddf505ca2cf2fa5cf7
권영달 <jiff100@funflow.co.kr>
2020-02-25 12:30:23 +0900

    UniRx - 샘플코드 추가

--- Unity/Assets/Script/UniRx/UniRxComponent.cs
+++ Unity/Assets/Script/UniRx/UniRxComponent.cs
@@ -1,17 +1,48 @@
 ﻿using System;
-using UnityEngine;
 using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class UniRxComponent : MonoBehaviour
 {
 
 	void Start()
 	{
+		/// EveryUpdate
 		var clickStream = Observable.EveryUpdate()
 			.Where(_ => Input.GetMouseButtonDown(0));
 
 		clickStream.Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
 			.Where(xs => xs.Count >= 2)
 			.Subscribe(xs => Debug.Log("DoubleClick Detected! Count:" + xs.Count));
+		////////////////////////////////////////////////////////////////////////////
+
+
+		/// ObservableWWW
+		ObservableWWW.Get("http://google.co.jp/").Subscribe(
+				x => Debug.Log(x.Substring(0, 100)), // onSuccess
+				ex => Debug.LogException(ex)); // onError
+
+		var parallel = Observable.WhenAll(
+			ObservableWWW.Get("http://google.com/"),
+			ObservableWWW.Get("http://bing.com/"),
+			ObservableWWW.Get("http://unity3d.com/"));
+
+		parallel.Subscribe(xs =>
+		{
+			Debug.Log(xs[0].Substring(0, 100)); // google
+			Debug.Log(xs[1].Substring(0, 100)); // bing
+			Debug.Log(xs[2].Substring(0, 100)); // unity
+		});
+		////////////////////////////////////////////////////////////////////////////
+
+
+		// notifier for progress use ScheduledNotifier or new Progress<float>(/* action */)
+		var progressNotifier = new ScheduledNotifier<float>();
+		progressNotifier.Subscribe(x => Debug.Log(x)); // write www.progress
+
+		// pass notifier to WWW.Get/Post
+		ObservableWWW.Get("http://google.com/", progress: progressNotifier).Subscribe();
 	}
 }
---
+++ Unity/Assets/Script/UniRx/UniRxCoroutineComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UniRx;
+
+public class UniRxCoroutineComponent : MonoBehaviour
+{
+	IEnumerator AsyncA()
+	{
+		Debug.Log("a start");
+		yield return new WaitForSeconds(1);
+		Debug.Log("a end");
+	}
+
+	IEnumerator AsyncB()
+	{
+		Debug.Log("b start");
+		yield return new WaitForEndOfFrame();
+		Debug.Log("b end");
+	}
+
+	void Start()
+	{
+		// main code
+		// Observable.FromCoroutine converts IEnumerator to Observable<Unit>.
+		// You can also use the shorthand, AsyncA().ToObservable()
+
+		// after AsyncA completes, run AsyncB as a continuous routine.
+		// UniRx expands SelectMany(IEnumerator) as SelectMany(IEnumerator.ToObservable())
+		var cancel = Observable.FromCoroutine(AsyncA)
+			.SelectMany(AsyncB)
+			.Subscribe();
+
+		// you can stop a coroutine by calling your subscription's Dispose.
+		cancel.Dispose();
+	}
+}
